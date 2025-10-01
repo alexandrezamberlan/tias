eeff6d5d1367ef3a6585b4b3e06c4b70b0d70f10
Alexandre de O. Zamberlan <alexz@ufn.edu.br>
2025-10-01 19:14:08 -0300

    Refactor diabetes management script with JSON handling

--- codigos_ia_generativa/codigoPython_trabalhoTIAS.py
+++ codigos_ia_generativa/codigoPython_trabalhoTIAS.py
@@ -3,25 +3,36 @@ from google import genai
 
 client = genai.Client(api_key="COLOQUE_SUA_API_KEY_AQUI")
 
-# O texto fornecido pelo usuário
-contexto = """
-sou diabetico tipo um, usuário de insulina lantus e insulina novorapid. Minha relação de insulina é de 15g por uma unidade. Para baixar a glicemia, uma unidade de insulina para 60 mg/dL.
+medicamentos = ['novorapid', 'basaglar']
+bolus_alimentar = 15 #15g de carboidrato por 1 unidade de insulina
+bolus_correcao = 60 #60mg/dL por 1 unidade de insulina
+glicemia_atual = 132
+
+descricao_alimentacao = """
+hoje de manhã comi um pão frances com uma fatia de queijo prato mais um ovo cozido. Também tomei um café com leite, mais café do que leite. 
 """
 
-pergunta = """
-hoje de manhã comi um pão frances com uma fatia de queijo prato mais um ovo cozido. Também tomei um café com leite, mais café do que leite. MInha glicemia em jejum foi de 129 mg/dL.
+contexto_json =  montar_json(medicamentos, bolus_alimentar, bolus_correcao, glicemia_atual, descricao_alimentacao)
+
+papel_esperado_ia = """
+Você é nutricionista especializada em contagem de carboidratos, calorias e cálculo da insulina necessária com base na alimentação e nos níveis de glicose informados
+"""
+
+resposta = """
+voce deve retornar SOMENTE um JSON contendo nome do alimento, quantidade de carboidrato, quantidade de caloria, quantidade de glicemia enviada e a quantidade de insulina necessária, tendo como base o JS
+
 """
 
 prompt = f"""
-voce é uma nutricionista especializada em contar carboidratos, calorias e calcular a necessidade de insulina a partir da alimentação informada e pela quantidade de glicose também informada.
-
-voce deve retornar SOMENTE um JSON contendo nome do alimento, quantidade de carboidrato, quantidade de caloria, quantidade de glicemia enviada e a quantidade de insulina necessária.
+Papel:
+{papel_esperado_ia}
 
 Contexto:
-{contexto}
+texto contextualizando o JSON de entrada.....
+{contexto_json}
 
-Pergunta:
-{pergunta}
+Resposta:
+{resposta}
 """
 
 response = client.models.generate_content(
@@ -29,19 +40,26 @@ response = client.models.generate_content(
         contents=prompt
 )
 
-json = response.candidates[0].content.parts[0].text.strip()
+resposta_json = response.candidates[0].content.parts[0].text.strip()
 
 # Função para calcular os tokens
 palavras = pergunta.split()
 palavras += contexto.split()
 palavras += json.split()
 palavras += str(response).split()
-
 num_palavras = len(palavras)
-
 # Estimativa: 1 palavra ≈ 1.33 tokens (média comum)
 tokens_estimados = int(num_palavras * 1.33)
-
 print(f"Tokens estimados: {tokens_estimados}")
 
-print(json)
+print(resposta_json)
+
+lista_alimentos, calorias, carboidratos, qtd_insulina = desmontar_json(resposta_json)
+
+
+
+
+
+
+
+x, y 
