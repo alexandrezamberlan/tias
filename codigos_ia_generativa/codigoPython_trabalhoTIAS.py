ad92c650c1cc07ea858da1f69eb60dd309d7d15d
Alexandre de O. Zamberlan <alexz@ufn.edu.br>
2025-10-03 08:33:46 -0300

    Update codigoPython_trabalhoTIAS.py

--- codigos_ia_generativa/codigoPython_trabalhoTIAS.py
+++ codigos_ia_generativa/codigoPython_trabalhoTIAS.py
@@ -3,9 +3,13 @@ from google import genai
 
 client = genai.Client(api_key="COLOQUE_SUA_API_KEY_AQUI")
 
+#dados clinicos do cliente
 medicamentos = ['novorapid', 'basaglar']
 bolus_alimentar = 15 #15g de carboidrato por 1 unidade de insulina
 bolus_correcao = 60 #60mg/dL por 1 unidade de insulina
+
+
+#dados do registro da refeição
 glicemia_atual = 132
 
 descricao_alimentacao = """
@@ -15,12 +19,11 @@ hoje de manhã comi um pão frances com uma fatia de queijo prato mais um ovo co
 contexto_json =  montar_json(medicamentos, bolus_alimentar, bolus_correcao, glicemia_atual, descricao_alimentacao)
 
 papel_esperado_ia = """
-Você é nutricionista especializada em contagem de carboidratos, calorias e cálculo da insulina necessária com base na alimentação e nos níveis de glicose informados
+Você é nutricionista especializada em contagem de carboidratos, calorias e cálculo da insulina necessária com base na alimentação e nos níveis de glicose informados.
 """
 
 resposta = """
-voce deve retornar SOMENTE um JSON contendo nome do alimento, quantidade de carboidrato, quantidade de caloria, quantidade de glicemia enviada e a quantidade de insulina necessária, tendo como base o JS
-
+voce deve retornar SOMENTE um JSON contendo todos os alimentos com nome do alimento, quantidade de carboidrato, quantidade de caloria, quantidade de glicemia enviada e a quantidade de insulina necessária.
 """
 
 prompt = f"""
@@ -28,7 +31,7 @@ Papel:
 {papel_esperado_ia}
 
 Contexto:
-texto contextualizando o JSON de entrada.....
+Utilize o JSON como fonte de entrada
 {contexto_json}
 
 Resposta:
